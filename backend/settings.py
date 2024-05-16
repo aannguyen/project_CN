f607cf4877e620f78ac91ab5957429d3725c31d4
AnPNT <aannguyen1405@gmail.com>
2024-05-16 14:06:08 +0700

    update source code

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'category',
+    'product',
+    'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -120,8 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'backend/static'
+]
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
---
+++ category/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+class CategoryList:
+    def get(self, request):
+        pass
